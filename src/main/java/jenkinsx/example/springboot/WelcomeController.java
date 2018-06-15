28d1e0ebf05a7ca0d4c57b4245b112c993bd9a31
Dennis Tait <dennis.tait@nanitesoftware.co.uk>
2018-06-15 13:50:19 +0100

    Fix: Updated welcome message

--- src/main/java/jenkinsx/example/springboot/WelcomeController.java
+++ src/main/java/jenkinsx/example/springboot/WelcomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WelcomeController {
 
 	@Value("${welcome.message:test}")
-	private String message = "Hello World";
+	private String message = "Hello World 1";
 
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
